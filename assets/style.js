718a2c8b63b902346039eac1f902d9b16c80b5e2
Julia <julia.gray30@gmail.com>
2022-01-29 11:20:13 +0000

    movie film actor function created

--- assets/style.js
+++ assets/style.js
@@ -1,12 +1,22 @@
 console.log("connected")
 // Fetches the IMDI data ----------------------------
 
+let movieSearch = "inception"
+
 async function getIMDBApi() {
-    let requestUrl = "https://imdb-api.com/en/API/FullCast/k_faz1hkma/Annie"
-    const response = await fetch(requestUrl)
-    const data = await response.json()
+    let requestUrl = `https://imdb-api.com/en/API/SearchMovie/k_faz1hkma/${movieSearch}`
+    let response = await fetch(requestUrl)
+    let data = await response.json()
     console.log(data)
+    let movieId = data.results[0].id
+    console.log(movieId)
+    let getFullCast = `https://imdb-api.com/en/API/FullCast/k_faz1hkma/${movieId}`
+    let response2 = await fetch(getFullCast)
+    let data2 = await response2.json()
+    console.log(data2)
 }
 
 getIMDBApi()
+
+
  
